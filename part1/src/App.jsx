72cd35b108809b97f74a009f4f58fa94e0c48ca9
Rajdeep <rajdeepchakraborty0611@gmail.com>
2024-04-04 00:14:49 +0530

    End of part1c

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,41 @@
 import {useState} from 'react'
+//App component
 const App=()=>{
+  //setting initial value of counter as 0 and assigning useState's fucntion attribute to setCounter
   const [counter,setCounter]=useState(0)
-  const increaseByOne=()=>{setCounter(counter +1)}
-  const setToZero=()=>{setCounter(0)}
+  console.log('rendering with the counter value',counter)
+
+  //event handler to increase value of counter by 1
+  const increaseByOne=()=>{
+    console.log('increasing,value before ',counter)
+    setCounter(counter +1)}
+  
+  //event handler to reset value of counter to 0
+  const setToZero=()=>{
+    console.log('setting to zero, value before ',counter)
+    setCounter(0)}
+  
+  //event handler to decrease value of counter by 1
+  const decreaseByOne=()=>{
+    console.log('decreasing, value before ',counter)
+    setCounter(counter-1)}
+  
+  //component to display value of counter
+  const Display=({counter})=><div><p>The value of counter is {counter}</p></div>
+  
+  //component to call event handler functions
+  const Buttons=({onClick,text})=><button onClick={onClick}>{text}</button>
+  
   return(
     <>
-    <div>{counter}</div>
-    <button onClick={increaseByOne}>
-      plus
-    </button>
-    <button onClick={setToZero}>
-      zero
-    </button>
+    <Display counter={counter}/>
+    
+    <Buttons onClick={increaseByOne} text="plus"/>
+    
+    <Buttons onClick={setToZero} text="zero"/>
+    
+    <Buttons onClick={decreaseByOne} text="minus"/>
+    
     </>
     
   )

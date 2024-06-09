cfac4f17ceac5b03a143a2f43000abebb6b7ee8c
Rajdeep <rajdeepchakraborty0611@gmail.com>
2024-06-09 15:01:05 +0530

    unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,21 +9,34 @@ const App=()=>{
   const [good,setGood] = useState(0)
   const [neutral,setNeutral] = useState(0)
   const [bad,setBad] = useState(0)
+  const [total,setTotal] = useState(0)
+  const [avg,setAvg] = useState(0)
+
   const goodclick=()=>{
     const updatedgood=good+1
     setGood(updatedgood)
+    const updatedtotal=total+1
+    setTotal(updatedtotal)
+    const updatedavg=avg+1
+    setAvg(updatedavg)
     console.log("no of good clicks" , updatedgood)
   }
 
   const neutralclick=()=>{
     const updatedneutral=neutral+1
     setNeutral(updatedneutral)
+    const updatedtotal=total+1
+    setTotal(updatedtotal)
     console.log("no of neutral clicks" , updatedneutral)
   } 
 
   const badclick=()=>{
     const updatedbad=bad+1
     setBad(updatedbad)
+    const updatedtotal=total+1
+    setTotal(updatedtotal)
+    const updatedavg=avg-1
+    setAvg(updatedavg)
     console.log("no of bad clicks" , updatedbad)
   }
   
@@ -36,7 +49,10 @@ const App=()=>{
       <h1>statistics</h1>
       <h3><p>good {good}</p>
       <p>neutral {neutral}</p>
-      <p>bad {bad}</p></h3>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {avg/total}</p>
+      <p>positive feedback {good*100/total}%</p></h3>
       
     </div>
     

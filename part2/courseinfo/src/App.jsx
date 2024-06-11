fd93ebd7b4c175361088b90cd5d2c3a78ec531e7
Rajdeep <rajdeepchakraborty0611@gmail.com>
2024-06-11 14:41:29 +0530

    courseinfo step 7 completed

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,83 +1,3 @@
-/*const Header=(props)=>{
-  return(
-    <div>
-    <h1>{props.course}</h1>
-  </div>
-  )
-}
-const Part1=(helo)=>{
-  return(
-    <div>
-      <p>
-        {helo.part1} {helo.exercises1}
-      </p>
-    </div>
-  )
-}
-const Part2=(helo)=>{
-  return(
-    <div>
-      <p>
-        {helo.part2} {helo.exercises2}
-      </p>
-    </div>
-  )
-}
-const Part3=(helo)=>{
-  return(
-    <div>
-      <p>
-        {helo.part3} {helo.exercises3}
-      </p>
-    </div>
-  )
-}
-const Content=(props)=>{
-  return(
-    <div>
-      <Part1 part1={props.parts[0].name} exercises1={props.parts[0].exercises}/>
-      
-      <Part2  part2={props.parts[1].name} exercises2={props.parts[1].exercises}/>
-      
-      <Part3  part3={props.parts[2].name} exercises3={props.parts[2].exercises}/>
-      
-    </div>
-  )
-}
-const Total=(props)=>{
-  return(
-    <p>Number of exercises {props.parts[0].exercises+ props.parts[1].exercises +props.parts[2].exercises}</p>
-  )
-}
-const App =()=>{
-  const course={
-    name : 'Half Stack application development',
-    parts : [
-    {
-      name:'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name:'State of a component',
-      exercises: 14
-    }
-  ]
-}
-    
-  return (
-    <>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
-    </>
-  )
-}
-export default App
-*/
 const Course =({course})=>{
   
   const Header=(props)=>{
@@ -88,47 +8,8 @@ const Course =({course})=>{
     )
   }
   
-  /*const Part1=(helo)=>{
-    return(
-      <div>
-        <p>
-          {helo.part1} {helo.exercises1}
-        </p>
-      </div>
-    )
-  }
-  
-  const Part2=(helo)=>{
-    return(
-      <div>
-        <p>
-          {helo.part2} {helo.exercises2}
-        </p>
-      </div>
-    )
-  }
-  
-  const Part3=(helo)=>{
-    return(
-      <div>
-        <p>
-          {helo.part3} {helo.exercises3}
-        </p>
-      </div>
-    )
-  }*/
-  
   const Content=({parts})=>{
-    /*return(
-      <div>
-        <Part1 part1={props.parts[0].name} exercises1={props.parts[0].exercises}/>
-        
-        <Part2  part2={props.parts[1].name} exercises2={props.parts[1].exercises}/>
-        
-        <Part3  part3={props.parts[2].name} exercises3={props.parts[2].exercises}/>
-        
-      </div>
-    )*/
+    
     return(
       <div>
         {parts.map(courses=>
@@ -139,10 +20,25 @@ const Course =({course})=>{
       </div>
     )
   }
+
+  const Total=({parts})=>{
+
+    var sum=0
+    {parts.map(courses=>
+      <p key={courses.id}>
+        {sum+=courses.exercises}
+      </p> )}
+    return(
+      <div>
+        Total number of exercises = {sum}
+      </div>
+    )
+  }
   return(
     <div>
       <Header course={course.name}/>
       <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
@@ -167,6 +63,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
